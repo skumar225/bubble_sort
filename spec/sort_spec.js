4ad73e6106bb1765e0d5e5170432844ea342328d
Elie Schoppik <eschoppik@gmail.com>
2015-03-14 19:13:16 -0700

    cleaning up tests

--- spec/sort_spec.js
+++ spec/sort_spec.js
@@ -3,7 +3,6 @@ var bubbleSort = require('../sort.js');
 describe('Bubble Sort', function () {
 
     it('should sort an array of numbers', function () {
-      expect([1,2]).toEqual([1,2])
       expect(bubbleSort([66,22,67,34])).toEqual([22,34,66,67]);
     });
     it('should return an array of a single number', function () {
