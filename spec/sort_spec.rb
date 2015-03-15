a28cca436d74b88a04a28e3dd2bbf557fcbdb8fd
Elie Schoppik <eschoppik@gmail.com>
2015-03-15 15:08:16 -0700

    cleaning up spec file

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
-require 'benchmark'
 require_relative '../lib/sort'
 
 describe "bubble_sort" do
-  it "sorts the array" do
+  it "sorts the array with a simple order" do
       expect([5,3,2,1,4].bubble_sort).to eq([1,2,3,4,5])
   end
 
-  it "sorts the array" do
+  it "sorts the array with negative numbers" do
+      expect([-5,3,-2,1,-4].bubble_sort).to eq([-5,-4,-2,1,3])
+  end
+
+  it "sorts the array with floats" do
+      expect([1.5,3.4,2,1,4].bubble_sort).to eq([1,1.5,2,3.4,4])
+  end
+
+  it "sorts the array with a randomized order" do
       expect((1..10).to_a.shuffle.bubble_sort).to eq((1..10).to_a)
   end
 end
