cbb87e40fd1aedb2eedb725afa5a9526201d53d5
Elie Schoppik <eschoppik@gmail.com>
2015-03-14 19:12:31 -0700

    adding starter code

--- sort.js
+++ sort.js
@@ -1,22 +1,5 @@
 function bubbleSort(arr){
-// iterative
-  var array_length = arr.length,
-    swapped,
-    swap;
 
-  do {
-    swapped = false;
-    for(var i=0 ; i < array_length; i++) {
-      if (arr[i] > arr[i+1]) {
-        swap = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = swap;
-        swapped = true;
-      }
-    }
-  } while (swapped === true);
-
-  return arr;
 }
 
 // do not touch this
